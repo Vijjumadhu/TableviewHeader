9a5d43aa2887566844144f3df7187c1f57ec50c7
Madhu Are <aremadhu8455@gmail.com>
2019-04-28 22:50:19 +0530

    Added Header and Tableview

---
+++ TableviewHeader/ListviewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListviewCell.swift
+//  TableviewHeader
+//
+//  Created by Serialxtech on 28/04/19.
+//  Copyright © 2019 madhu.are. All rights reserved.
+//
+
+import UIKit
+
+class ListviewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- TableviewHeader/ViewController.swift
+++ TableviewHeader/ViewController.swift
@@ -8,11 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
+{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+         let cell = tableView.dequeueReusableCell(withIdentifier: "ListviewCell", for: indexPath) as! ListviewCell
+        cell.textLabel?.text = names[indexPath.row]
+        return cell
+    }
+    
 
+    @IBOutlet var header_view: UIView!
+    @IBOutlet weak var tableviewobj: UITableView!
+    var names = ["one","two","three","four","five","six"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableviewobj.rowHeight = UITableView.automaticDimension
+        tableviewobj.estimatedRowHeight = UITableView.automaticDimension
+        self.tableviewobj.tableHeaderView = self.header_view
     }
 
 
